9b778f96bcb18f2d3b97665e0464e7b36e523479
My Tran <mytran@yahoo-inc.com>
2016-09-20 16:45:45 -0700

    Adding main() and change the length value based on wordArray, instead of str

--- src/main/java/com/algorithm/week19/ReverseStringByWord.java
+++ src/main/java/com/algorithm/week19/ReverseStringByWord.java
@@ -8,11 +8,17 @@ public class ReverseStringByWord {
 		
 		// Split string in array of word by space
 		String[] wordArray = str.split("\\s");
-		int length = str.length();
+		int length = wordArray.length;
 		for (int index = length - 1; index >= 0; index --) {
 			sb.append(wordArray[index]).append(" ");
 		}
 		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		String str = "This is a test.";
+		System.out.println("Before reverse: " + str);
+		System.out.println("After reverse: " + reverseStringByWord(str));
 	}
 
 }
